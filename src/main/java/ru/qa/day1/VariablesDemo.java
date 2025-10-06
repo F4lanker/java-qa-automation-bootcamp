b74a2f1d0ddb4c697c4a6ee5ea87b4d0dc937a4e
Nikolai <n.berezovskii@magdv.com>
2025-10-06 17:34:01 +0700

    feat: improve NumberClassifier logic

---
+++ src/main/java/ru/qa/day1/VariablesDemo.java
@@ -0,0 +1,11 @@
+package ru.qa.day1;
+
+public class VariablesDemo {
+    public static void main(String[] args) {
+        byte age = 25;
+        int salary = 100_000;
+        String name = "Tester";
+
+        System.out.println("Привет, " + name + "! Тебе " + age + ", зарплата: " + salary);
+    }
+}
