08b05cf1c63c30ae44845fecda77f9cce24f23e5
Nikolai <n.berezovskii@magdv.com>
2025-10-07 11:21:39 +0700

    feat: refactor the code, now covered String values as well

--- src/main/java/ru/qa/day1/NumberClassifier.java
+++ src/main/java/ru/qa/day1/NumberClassifier.java
@@ -1,14 +1,17 @@
 package ru.qa.day1;
 
-public static String classify(Object input){
-    if(input == null){
-        return "значение не задано";
-    }
+public class NumberClassifier {
+    public static String classify(Object input) {
+        if (input == null) {
+            return "значение не задано";
+        }
+        if(input.getClass() == String.class) return "некорректное значение";
 
-    if (input instanceof Number number){
-        double value = number.doubleValue();
-        if(value>0) return "положительное";
-        else if (value<0) return "отрицательное";
+        if (input instanceof Number number) {
+            double value = number.doubleValue();
+            if (value > 0) return "положительное";
+            else if (value < 0) return "отрицательное";
+        }
         return "ноль";
     }
 }
