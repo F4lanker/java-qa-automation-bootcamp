29647d1891a612c5990d23ab31a52218e08c379a
Nikolai <n.berezovskii@magdv.com>
2025-10-08 11:26:14 +0700

    feat: refactor folders for StatusMessageGenerator to correct Day3

--- src/main/java/ru/qa/day3/StatusMessageGenerator.java
+++ src/main/java/ru/qa/day3/StatusMessageGenerator.java
@@ -1,4 +1,22 @@
 package ru.qa.day3;
 
 public class StatusMessageGenerator {
+    // Реализуй двумя способами
+    public static String getStatusMessageIfVersion(int statusCode) {
+            if (statusCode == 200) return "OK";
+            if (statusCode == 404) return "Not Found";
+            if (statusCode == 400) return "Bad Request";
+            if (statusCode == 500) return "Server Error";
+            else return "Unknown";
+    }
+    public static String getStatusMessageSwitchCase(int statusCode) {
+        return switch (statusCode) {
+            case 200 -> "OK";
+            case 404 -> "Not Found";
+            case 400 -> "Bad Request";
+            case 500 -> "Server Error";
+            default -> "Unknonwn";
+        };
+    }
+
 }
