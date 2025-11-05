81f3aa1a5caf160748dc52cd38d23b32a77c4636
Nikolai <n.berezovskii@magdv.com>
2025-11-05 10:37:58 +0700

    data: ListSamplesForTest refactor constant names to Upper Snake case

--- src/test/java/testData/ListSamplesForTests.java
+++ src/test/java/testData/ListSamplesForTests.java
@@ -2,21 +2,22 @@ package testData;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class ListSamplesForTests {
     //Empty list
-    public static final List <String> emptyList = new ArrayList<>();
+    public static final List <String> EMPTIED_LIST = Collections.emptyList();
     //List consists NULL
-    public static final List<String> nullContainList = Arrays.asList((String) null);
+    public static final List<String> NULL_CONTAIN_LIST =Collections.unmodifiableList(Arrays.asList((String) null));
     //List consist NULL and another date
-    public static final List<String> nullAndOtherDataList = Arrays.asList((String) null, "Alice");
+    public static final List<String> NULL_AND_OTHER_DATA_LIST = Collections.unmodifiableList(Arrays.asList((String) null, "Alice"));
     // NULL-link (no list)
-    public static final List<String> nullList = null;
+    public static final List<String> NULL_LIST = null;
     //One element list
-    public static final List<String> oneElementList = new ArrayList<String>(List.of("alfa"));
+    public static final List<String> ONE_ELEMENT_LIST = new ArrayList<String>(List.of("alfa"));
     //Ten elements list
-    public static final List<String> tenElements = new ArrayList<String>(List.of("Sample", "String", "Alice", "a@test.com", " ", "Bob", "b@test.com", "refix", "Index", "Paris" ));
+    public static final List<String> TEN_ELEMENTS = new ArrayList<String>(List.of("Sample", "String", "Alice", "a@test.com", " ", "Bob", "b@test.com", "refix", "Index", "Paris" ));
     // Repeat element list
-    public static final List<String> repeatElements = new ArrayList<String>(List.of("Sample", "Sample", "Sample" ));
+    public static final List<String> REPEAT_ELEMENTS = new ArrayList<String>(List.of("Sample", "Sample", "Sample" ));
 }
