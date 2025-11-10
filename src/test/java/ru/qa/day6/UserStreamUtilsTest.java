f661555ba739080d252044acc913e982d81460c8
Nikolai <n.berezovskii@magdv.com>
2025-11-10 11:08:12 +0700

    feat: refacto filter adult adding null values as option

---
+++ src/test/java/ru/qa/day6/UserStreamUtilsTest.java
@@ -0,0 +1,12 @@
+package ru.qa.day6;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class UserStremUtilsTest {
+    @Test
+    @DisplayName("test function returns user whose age is abobe 18")
+    void userAgeAbove18Test(){
+        asser
+    }
+}
