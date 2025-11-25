1888b1fa0b822e0785a32f493db9f9ab34b60059
Nikolai <n.berezovskii@magdv.com>
2025-11-25 11:27:26 +0700

    feat: add StreamUtils.groupByAge; refactor: annotaions added

--- src/main/java/ru/qa/day7/StreamUtils.java
+++ src/main/java/ru/qa/day7/StreamUtils.java
@@ -2,12 +2,11 @@ package ru.qa.day7;
 
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.jetbrains.annotations.Unmodifiable;
+import ru.qa.day4.User;
 
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class StreamUtils {
@@ -15,7 +14,8 @@ public class StreamUtils {
     private StreamUtils(){ throw new AssertionError("Нельзя создавать пустой экземляр утилитного класса");
     }
     @Contract("null -> _; _ -> _")
-    public static @NotNull @Unmodifiable List<String> toUpperCase(List<String> words){
+    @NotNull
+    public static @Unmodifiable List<String> toUpperCase(@Nullable List<String> words){
         if(words == null || words.isEmpty()){
             return Collections.emptyList();
         }
@@ -24,5 +24,14 @@ public class StreamUtils {
                 .map(String::toUpperCase)
                 .collect(Collectors.toCollection(HashSet::new))
                 .stream().toList();
+    }
+
+    @Contract("null -> !null")
+    public static Map<Integer, List<User>> groupByAge(List<User> users){
+        if(users == null || users.isEmpty()){
+            return Collections.emptyMap();
+        }
+        return users.stream()
+                .collect(Collectors.groupingBy(User::getAge));
     }
 }
