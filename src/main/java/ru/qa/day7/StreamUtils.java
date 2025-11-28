772a520d9a39709641745404a6e71be9c45cdc58
Nikolai <n.berezovskii@magdv.com>
2025-11-28 10:41:46 +0700

    feat: add StreamUtils.getAgeStatistics. refatcor:comments added, code reformated

--- src/main/java/ru/qa/day7/StreamUtils.java
+++ src/main/java/ru/qa/day7/StreamUtils.java
@@ -11,12 +11,15 @@ import java.util.stream.Collectors;
 
 public class StreamUtils {
     @Contract(" -> fail")
-    private StreamUtils(){ throw new AssertionError("Нельзя создавать пустой экземляр утилитного класса");
+    private StreamUtils() {
+        throw new AssertionError("Нельзя создавать пустой экземляр утилитного класса");
     }
+
+    // function returns User list in Upper Case removing duplicates
     @Contract("null -> _; _ -> _")
     @NotNull
-    public static @Unmodifiable List<String> toUpperCase(@Nullable List<String> words){
-        if(words == null || words.isEmpty()){
+    public static @Unmodifiable List<String> toUpperCase(@Nullable List<String> words) {
+        if (words == null || words.isEmpty()) {
             return Collections.emptyList();
         }
         return words.stream()
@@ -26,12 +29,39 @@ public class StreamUtils {
                 .stream().toList();
     }
 
+    // function returns dictionary with users grouped by age
     @Contract("null -> !null")
-    public static Map<Integer, List<User>> groupByAge(List<User> users){
-        if(users == null || users.isEmpty()){
+    @NotNull
+    public static Map<Integer, List<User>> groupByAge(List<User> users) {
+        if (users == null || users.isEmpty()) {
             return Collections.emptyMap();
         }
         return users.stream()
                 .collect(Collectors.groupingBy(User::getAge));
+    }
+
+    // function returns group of adults and non adults
+    @Contract("null -> !null")
+    @NotNull
+    public static @Unmodifiable Map<Boolean, List<User>> partitionByAdultStatus(List<User> users) {
+        if (users == null || users.isEmpty()) {
+            return Collections.emptyMap();
+        }
+        return users.stream()
+                .filter(Objects::nonNull)
+                .collect(Collectors.partitioningBy(
+                        user -> user.getAge() >= 18));
+    }
+
+    // returns age statistics
+    @Contract("null -> !null")
+    @NotNull
+    public static DoubleSummaryStatistics getAgeStatistics(List<User> users) {
+        if (users == null || users.isEmpty()) {
+            return new DoubleSummaryStatistics();
+        }
+        return users.stream()
+                .filter(Objects::nonNull)
+                .collect(Collectors.summarizingDouble(User::getAge));
     }
 }
