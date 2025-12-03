dedcdd375edabef76d587459c5dafd43639024bc
Nikolai <n.berezovskii@magdv.com>
2025-12-03 12:00:13 +0700

    Refactor: test: day7/StreamUtils.java @;Nested annotations, test partitioning

--- src/test/java/ru/qa/day7/StreamUtilsTest.java
+++ src/test/java/ru/qa/day7/StreamUtilsTest.java
@@ -1,6 +1,7 @@
 package ru.qa.day7;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.*;
@@ -10,21 +11,42 @@ import static ru.qa.day7.StreamUtils.toUpperCase;
 import static testData.ListSamplesForTests.*;
 
 public class StreamUtilsTest {
-    @DisplayName("Function returns List of inserted string in UPPER CASE")
-    @Test
-    void toUpperCaseTest(){
-        Set<String> tenElementsResult = Set.of("SAMPLE", "STRING", "ALICE", "A@TEST.COM"," ", "BOB", "B@TEST.COM", "REFIX", "INDEX", "PARIS");
-        Set<String> tenElementsActual = new HashSet<>(toUpperCase(TEN_ELEMENTS));
-        Set<String> nullAndOtherElementsResult = Set.of("ALICE");
-        Set<String> nullAndOtherElementsActual = new HashSet<>(toUpperCase(NULL_AND_OTHER_DATA_LIST));
+    @Nested
+    @DisplayName("Test toUpperCase")
+    class ToUppercaseTests{
 
-        assertAll(
-                ()-> assertEquals(tenElementsResult, tenElementsActual),
-                ()-> assertEquals(Collections.emptyList(), toUpperCase(NULL_LIST)),
-                ()-> assertEquals(Collections.emptyList(), toUpperCase(EMPTIED_LIST)),
-                ()-> assertEquals(nullAndOtherElementsResult, nullAndOtherElementsActual),
-                ()-> assertTrue(toUpperCase(REPEAT_ELEMENTS).contains("SAMPLE")),
-                ()-> assertEquals(1, toUpperCase(REPEAT_ELEMENTS).size())
-        );
-    }
+        @DisplayName("Should return the List of inserted string in UPPER CASE")
+        @Test
+        void shouldConvertToUpperCase(){
+            List<String> input = List.of("sample", "world");
+            Set<String> result = Set.of("SAMPLE", "WORLD");
+            assertEquals(result, new HashSet<>(toUpperCase(input)));
+        }
+
+        @DisplayName("Should remove duplicates")
+        void shouldRemoveDuplicates(){
+            assertAll(
+                    ()-> assertTrue(toUpperCase(REPEAT_ELEMENTS).contains("SAMPLE")),
+                    ()-> assertEquals(1, toUpperCase(REPEAT_ELEMENTS).size())
+            );
+        }
+
+        @DisplayName("Should return empty list if input is NULL or Empty list")
+        @Test
+        void nullInput(){
+            assertAll(
+                    ()-> assertEquals(Collections.emptyList(), toUpperCase(NULL_LIST)),
+                    ()-> assertEquals(Collections.emptyList(), toUpperCase(EMPTIED_LIST))
+            );
+        }
+
+        @DisplayName("Should return values in upper case if one of the value in the list is NULL")
+        @Test
+        void nullInTheList(){
+            Set<String> nullAndOtherElementsResult = Set.of("ALICE");
+            Set<String> nullAndOtherElementsActual = new HashSet<>(toUpperCase(NULL_AND_OTHER_DATA_LIST));
+
+            assertEquals(nullAndOtherElementsActual, nullAndOtherElementsResult);
+        }
+}
 }
