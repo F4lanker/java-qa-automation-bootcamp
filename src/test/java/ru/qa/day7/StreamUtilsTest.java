1ec7adc6412c984c580aa7f2251d9392463c6a1c
Nikolai <n.berezovskii@magdv.com>
2025-12-10 13:37:02 +0700

    refactor: code formated

--- src/test/java/ru/qa/day7/StreamUtilsTest.java
+++ src/test/java/ru/qa/day7/StreamUtilsTest.java
@@ -16,11 +16,11 @@ import static testData.UsersListsSample.*;
 public class StreamUtilsTest {
     @Nested
     @DisplayName("Test toUpperCase")
-    class ToUppercaseTests{
+    class ToUppercaseTests {
 
         @DisplayName("Should return the List of inserted string in UPPER CASE")
         @Test
-        void shouldConvertToUpperCase(){
+        void shouldConvertToUpperCase() {
             List<String> input = List.of("sample", "world");
             Set<String> result = Set.of("SAMPLE", "WORLD");
             assertEquals(result, new HashSet<>(toUpperCase(input)));
@@ -28,44 +28,45 @@ public class StreamUtilsTest {
 
         @DisplayName("Should remove duplicates")
         @Test
-        void shouldRemoveDuplicates(){
+        void shouldRemoveDuplicates() {
             assertAll(
-                    ()-> assertTrue(toUpperCase(REPEAT_ELEMENTS).contains("SAMPLE")),
-                    ()-> assertEquals(1, toUpperCase(REPEAT_ELEMENTS).size())
+                    () -> assertTrue(toUpperCase(REPEAT_ELEMENTS).contains("SAMPLE")),
+                    () -> assertEquals(1, toUpperCase(REPEAT_ELEMENTS).size())
             );
         }
 
         @DisplayName("Should return empty list if input is NULL or Empty list")
         @Test
-        void nullInput(){
+        void nullInput() {
             assertAll(
-                    ()-> assertEquals(Collections.emptyList(), toUpperCase(NULL_LIST)),
-                    ()-> assertEquals(Collections.emptyList(), toUpperCase(EMPTIED_LIST))
+                    () -> assertEquals(Collections.emptyList(), toUpperCase(NULL_LIST)),
+                    () -> assertEquals(Collections.emptyList(), toUpperCase(EMPTIED_LIST))
             );
         }
 
         @DisplayName("Should return values in upper case if one of the value in the list is NULL")
         @Test
-        void nullInTheList(){
+        void nullInTheList() {
             Set<String> nullAndOtherElementsResult = Set.of("ALICE");
             Set<String> nullAndOtherElementsActual = new HashSet<>(toUpperCase(NULL_AND_OTHER_DATA_LIST));
 
             assertEquals(nullAndOtherElementsActual, nullAndOtherElementsResult);
         }
-}
+    }
+
     @DisplayName("Test groupByAge")
     @Nested
     class GroupedByAge {
 
         @DisplayName("Should return dictionary with users gouped by Age")
         @Test
-        void positiveCase(){
-            Map <Integer, List<User>> expected = Map.of(
+        void positiveCase() {
+            Map<Integer, List<User>> expected = Map.of(
                     17, List.of(TEENAGE_USER),
                     1, List.of(USER_WITH_EMPTY_STRINGS)
             );
             Map<Integer, List<User>> expetedSameAge = Map.of(
-                    17, List.of(TEENAGE_USER,TEENAGE_USER_TWO)
+                    17, List.of(TEENAGE_USER, TEENAGE_USER_TWO)
             );
 
             assertEquals(expected, groupByAge(TEENAGE_USER_LIST));
@@ -74,24 +75,24 @@ public class StreamUtilsTest {
 
         @DisplayName("Should return empty list if inptuted list is NULL")
         @Test
-        void nullCase(){
+        void nullCase() {
             assertEquals(Collections.emptyMap(), groupByAge(NULL_USER_LIST));
         }
 
         @DisplayName("Should return Empty list, When inputed User list is empty")
         @Test
-        void emptyListCase(){
+        void emptyListCase() {
             assertEquals(Collections.emptyMap(), groupByAge(EMPTY_LIST));
         }
     }
 
     @DisplayName("Test partitionByAdultStatus")
     @Nested
-    class PartitionByAdultStatus{
+    class PartitionByAdultStatus {
 
         @DisplayName("Should return Map of adults and non adults Usres")
         @Test
-        void positiveCase(){
+        void positiveCase() {
             Map<Boolean, List<User>> expectedUserList = Map.of(
                     true, List.of(VALID_USER, NULL_VALUE_IN_STRING, SIMILAR_PREFIX_IN_NAME),
                     false, List.of(USER_WITH_EMPTY_STRINGS, TEENAGE_USER, SIMILAR_PREFIX_IN_NAME_2)
@@ -107,64 +108,64 @@ public class StreamUtilsTest {
 
         @DisplayName("Should return empty map if input is Empty or NULL")
         @Test
-        void emptyNullTest(){
+        void emptyNullTest() {
             assertAll(
-                    ()->assertEquals(Collections.emptyMap(), partitionByAdultStatus(EMPTY_LIST)),
-                    ()-> assertEquals(Collections.emptyMap(), partitionByAdultStatus(NULL_USER_LIST))
+                    () -> assertEquals(Collections.emptyMap(), partitionByAdultStatus(EMPTY_LIST)),
+                    () -> assertEquals(Collections.emptyMap(), partitionByAdultStatus(NULL_USER_LIST))
             );
         }
     }
 
     @DisplayName("Test getAgeStatistic")
     @Nested
-    class AgeStatistic{
+    class AgeStatistic {
 
         @DisplayName("Should return statistic against inputed list")
         @Test
-        void positiveCase(){
+        void positiveCase() {
             assertAll(
-                    ()-> assertEquals(3, getAgeStatistics(ONE_USER_18YO).getCount(), 0.001), // there are 3 users in the list
-                    ()-> assertEquals(1, getAgeStatistics(ONE_USER_18YO).getMin(), 0.001), // USER_WITH_EMPTY_STRING age = 1
-                    ()-> assertEquals(18, getAgeStatistics(ONE_USER_18YO).getMax(), 0.001), // NULL_VALUE_IN_STRINGS age = 18
-                    ()-> assertEquals(12, getAgeStatistics(ONE_USER_18YO).getAverage(), 0.001) // average age is 12 y.o = (18 + 17 + 1) / 3
+                    () -> assertEquals(3, getAgeStatistics(ONE_USER_18YO).getCount(), 0.001), // there are 3 users in the list
+                    () -> assertEquals(1, getAgeStatistics(ONE_USER_18YO).getMin(), 0.001), // USER_WITH_EMPTY_STRING age = 1
+                    () -> assertEquals(18, getAgeStatistics(ONE_USER_18YO).getMax(), 0.001), // NULL_VALUE_IN_STRINGS age = 18
+                    () -> assertEquals(12, getAgeStatistics(ONE_USER_18YO).getAverage(), 0.001) // average age is 12 y.o = (18 + 17 + 1) / 3
             );
         }
 
         @DisplayName("Should return statistics if there is only one user in the list")
         @Test
-        void oneUserTest(){
+        void oneUserTest() {
             List<User> input = List.of(TEENAGE_USER); //(4, "Bob", "bob@test.com", 17);
 
             assertAll(
-                    ()-> assertEquals(1, getAgeStatistics(input).getCount(), 0.001),//there is the only one user in the list
-                    ()-> assertEquals(17, getAgeStatistics(input).getMin(), 0.001), //MIN, MAX and average  age is the same
-                    ()-> assertEquals(17, getAgeStatistics(input).getMax(), 0.001),
-                    ()-> assertEquals(17, getAgeStatistics(input).getAverage(), 0.001)
+                    () -> assertEquals(1, getAgeStatistics(input).getCount(), 0.001),//there is the only one user in the list
+                    () -> assertEquals(17, getAgeStatistics(input).getMin(), 0.001), //MIN, MAX and average  age is the same
+                    () -> assertEquals(17, getAgeStatistics(input).getMax(), 0.001),
+                    () -> assertEquals(17, getAgeStatistics(input).getAverage(), 0.001)
             );
         }
 
 
         @DisplayName("Should return statistics if list is NULL or Empty")
         @Test
-        void emptyNullTest(){
+        void emptyNullTest() {
             DoubleSummaryStatistics emptyStat = getAgeStatistics(EMPTY_LIST);
             DoubleSummaryStatistics nullStat = getAgeStatistics(NULL_USER_LIST);
 
             assertAll(
-                    ()-> assertEquals(0, emptyStat.getCount()),
+                    () -> assertEquals(0, emptyStat.getCount()),
                     () -> assertEquals(0, emptyStat.getAverage(), 0.001),
-                    ()-> assertEquals(Double.POSITIVE_INFINITY, emptyStat.getMin()),
+                    () -> assertEquals(Double.POSITIVE_INFINITY, emptyStat.getMin()),
                     () -> assertEquals(Double.NEGATIVE_INFINITY, emptyStat.getMax())
             );
 
             assertAll(
-                    ()-> assertEquals(0, nullStat.getCount()),
+                    () -> assertEquals(0, nullStat.getCount()),
                     () -> assertEquals(0, nullStat.getAverage(), 0.001),
-                    ()-> assertEquals(Double.POSITIVE_INFINITY, nullStat.getMin()),
+                    () -> assertEquals(Double.POSITIVE_INFINITY, nullStat.getMin()),
                     () -> assertEquals(Double.NEGATIVE_INFINITY, nullStat.getMax())
             );
         }
 
-            }
-        }
+    }
+}
 
