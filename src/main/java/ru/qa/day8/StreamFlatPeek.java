b397ad1ea373e7d5418416a56535679dbfe29487
Nikolai <n.berezovskii@magdv.com>
2025-12-16 12:25:53 +0700

    refactor: day8/StreamFlatPeek.flattenUserEmail

--- src/main/java/ru/qa/day8/StreamFlatPeek.java
+++ src/main/java/ru/qa/day8/StreamFlatPeek.java
@@ -8,20 +8,24 @@ import ru.qa.day4.User;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 
 public class StreamFlatPeek {
     @Contract(" -> fail")
     private StreamFlatPeek(){throw new AssertionError("Нельзя создавать пустой экземляр утилитного класса");}
 
+    // return the flat list of email from (List of List of Users)
     @Contract("null -> _; _ -> _")
     @NotNull
     public static @Unmodifiable List<String> flattenUserEmails(List<List<User>> userGroups){
-        if(userGroups == null || userGroups.isEmpty()){
-            return Collections.emptyList();
-        }
-        return userGroups.stream()
-                .filter(Objects::nonNull)
-                .flatMap(user -> user.stream().map(User::getEmail))
+        return Optional.ofNullable(userGroups)
+                .orElse(Collections.emptyList())
+                .stream()
+                .filter(Objects::nonNull)                               // filter lists
+                .flatMap(user -> user.stream()
+                        .filter(Objects::nonNull)                       //filter null users
+                        .map(User::getEmail)
+                        .filter(Objects::nonNull))                     //filter null in Emails
                 .toList();
     }
 
