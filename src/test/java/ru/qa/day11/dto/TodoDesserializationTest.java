bbce7fed445f22b9c334f879e8497d178c037be5
Nikolai <n.berezovskii@magdv.com>
2026-03-03 10:39:53 +0700

    feat: day11.TodoDesserializationTest - deserializeTodosList() added

--- src/test/java/ru/qa/day11/dto/TodoDesserializationTest.java
+++ src/test/java/ru/qa/day11/dto/TodoDesserializationTest.java
@@ -1,11 +1,14 @@
 package ru.qa.day11.dto;
 
 import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.common.mapper.TypeRef;
 import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.*;
@@ -22,7 +25,7 @@ public class TodoDesserializationTest {
     }
 
     @Test
-    @DisplayName("Should return POJO as response")
+    @DisplayName("/todos/1 - Should return POJO as response")
     void deserializeTodo() {
         Todo response = given()
                 .spec(spec)
@@ -33,10 +36,28 @@ public class TodoDesserializationTest {
                 .log().ifValidationFails()
                 .extract().body().as(Todo.class);
         Assertions.assertAll(
-                ()-> assertEquals(1, response.getId()),
-                ()-> assertNotNull(response.getUserId()),
-                ()-> assertFalse(response.isCompleted())
-        );
+                () -> assertEquals(1, response.getId()),
+                () -> assertNotNull(response.getUserId()),
+                () -> assertFalse(response.isCompleted())
+                            );
 
+    }
+
+    @Test
+    @DisplayName("/todos - Should return the list Todos")
+    void deserializeTodosList() {
+        List<Todo> response = given()
+                .spec(spec)
+                .log().ifValidationFails()
+                .when()
+                .get("/todos")
+                .then()
+                .log().ifValidationFails()
+                .extract().body().as(new TypeRef<List<Todo>>() {});
+        Assertions.assertAll(
+                ()-> assertFalse(response.isEmpty()),
+                ()-> assertNotNull(response.get(0)),
+                ()-> assertEquals(response.get(0).getId(), 1)
+                            );
     }
 }
