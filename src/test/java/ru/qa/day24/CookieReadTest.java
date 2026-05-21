d6fdd4435997b5ecaa43c3f94da06f4caf8eb4b2
Nikolai <n.berezovskii@magdv.com>
2026-05-21 13:01:35 +0700

    WIP: day24/CookieReadTest

---
+++ src/test/java/ru/qa/day24/CookieReadTest.java
@@ -0,0 +1,67 @@
+package ru.qa.day24;
+
+import io.restassured.filter.cookie.CookieFilter;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.config;
+import static io.restassured.RestAssured.given;
+import static io.restassured.config.RedirectConfig.redirectConfig;
+import static org.hamcrest.Matchers.equalTo;
+import static ru.qa.specs.ApiSpecs.*;
+
+public class CookieReadTest {
+    @Test
+    @DisplayName("GET: Querry params returned in cookies")
+    void shouldReceiveCookieFromServer() {
+        String sessionId = "abc123";
+        given()
+                .spec(httpBinRequestSpec())
+                .queryParam("sessionId", sessionId)
+                .config(config().redirect(redirectConfig().followRedirects(false))) //without redirect only read cookies is available
+                .when()
+                .get("/cookies/set")
+                .then()
+                .log().all()
+                .cookie("sessionId", sessionId);
+    }
+
+    @Test
+    @DisplayName("GET: baseURL/cookies returns sent cookie")
+    void shouldSendCookieToServer(){
+        String token = "mySecretToken123";
+        given()
+        .spec(httpBinRequestSpec())
+                .cookie("token", token)
+                .when()
+                .get("/cookies")
+        .then()
+                .log().all()
+                .body("cookies.token", equalTo(token));
+    }
+
+    @Test
+    void shouldMaintainSessionAcrossRequests(){
+        CookieFilter cookieFilter = new CookieFilter(); // change from required SessionFilter to Cookie filter as Session can't read custom cookie value as user
+        String user = "nikolai";
+
+        given()
+                .spec(httpBinRequestSpec())
+                .queryParam("user", user)
+                .filter(cookieFilter)
+                .config(config().redirect(redirectConfig().followRedirects(false))) // have to switch off redirect to read cookie
+                .log().all()
+                .when()
+                .get("/cookies/set")
+                .then()
+                .log().all();
+
+        given()
+                .spec(httpBinRequestSpec())
+                .filter(cookieFilter)
+                .when()
+                .get("/cookies")
+                .then()
+                .body("cookies.user", equalTo(user));
+    }
+}
