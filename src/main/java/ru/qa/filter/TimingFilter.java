0d95a4e53210d563be0c5dc1d872146ec13fdf18
Nikolai <n.berezovskii@magdv.com>
2026-05-29 15:49:29 +0700

    feat: TimingFilter.java has been added

--- src/main/java/ru/qa/filter/TimingFilter.java
+++ src/main/java/ru/qa/filter/TimingFilter.java
@@ -1,4 +1,35 @@
 package ru.qa.filter;
 
-public class TimingFilter {
+import io.restassured.filter.Filter;
+import io.restassured.filter.FilterContext;
+import io.restassured.response.Response;
+import io.restassured.specification.FilterableRequestSpecification;
+import io.restassured.specification.FilterableResponseSpecification;
+
+public class TimingFilter implements Filter {
+    private final long thresholdMs;
+
+    public TimingFilter(long thresholdMs) {
+        this.thresholdMs = thresholdMs;
+    }
+
+    @Override
+    public Response filter(FilterableRequestSpecification requestSpec, FilterableResponseSpecification responseSpec, FilterContext ctx) {
+
+        //Track response starting
+        long timeStart = System.currentTimeMillis();
+
+        // Perform real request and get the response
+        Response response = ctx.next(requestSpec, responseSpec);
+        long duration = System.currentTimeMillis() - timeStart;
+
+        if(duration > thresholdMs) {
+          throw new AssertionError(
+                  String.format("Request exceeded threshold: %dms > %dms", duration, thresholdMs)
+          );
+
+        }
+        return response;
+    }
+
 }
