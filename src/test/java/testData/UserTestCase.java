77daf64753faa2927e5aaa4a1bcc4e6a4d4b8f48
Nikolai <n.berezovskii@magdv.com>
2026-07-09 11:15:39 +0700

    test: add test data - UestTestCase and users.json

--- src/test/java/testData/UserTestCase.java
+++ src/test/java/testData/UserTestCase.java
@@ -1,4 +1,12 @@
 package testData;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class UserTestCase {
+    private String name;
+    private String email;
+    private int expectedStatus;
 }
