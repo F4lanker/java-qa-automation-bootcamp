44ca33f760c883389ee82f4b43fb8571524afea3
Nikolai <n.berezovskii@magdv.com>
2026-07-21 10:36:10 +0700

    test: add schema validation and support null body

--- src/test/java/ru/qa/day29/PostsCsvDrivenTest.java
+++ src/test/java/ru/qa/day29/PostsCsvDrivenTest.java
@@ -1,12 +1,15 @@
 package ru.qa.day29;
 
 import io.qameta.allure.Feature;
+import io.qameta.allure.Step;
 import io.qameta.allure.Story;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import ru.qa.dto.CreatePostRequest;
+import ru.qa.util.SchemaValidatorUtil;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
@@ -18,14 +21,15 @@ public class PostsCsvDrivenTest {
     @DisplayName("Create post")
     @ParameterizedTest(name = " > {0}")
     @CsvFileSource(resources = "/testData/UserTestCase/posts.csv", numLinesToSkip = 1)
+    @Step("POST /posts → title={1}, userId={0}")
     void shouldCreatePostsFromCsv(String scenario, int userId, String title, String body) {
         CreatePostRequest request = CreatePostRequest.builder()
                                                      .title(title)
-                                                     .body(body)
+                                                     .body(body != null ? body : "")
                                                      .userId(userId)
                                                      .build();
 
-        given()
+        Response response =  given()
                 .spec(baseRequestSpec())
                 .contentType(ContentType.JSON)
                 .body(request)
@@ -33,6 +37,9 @@ public class PostsCsvDrivenTest {
                 .post("/posts")
                 .then()
                 .spec(successResponseSpec())
-                .body("title", equalTo(title));
+                .body("title", equalTo(title))
+                .extract().response();
+
+        SchemaValidatorUtil.checkSavedJsonSchema(response.asString(), "schemas/post-schema.json");
     }
 }
