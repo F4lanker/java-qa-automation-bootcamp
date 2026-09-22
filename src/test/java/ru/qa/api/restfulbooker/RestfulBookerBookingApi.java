5fe36ad8e99512a12fd6b28d73df559349506551
Nikolai <n.berezovskii@magdv.com>
2026-09-22 11:19:23 +0700

    refactor/day31:Move the methods for GET and PUT in RestfulBookerBookingApi

--- src/test/java/ru/qa/api/restfulbooker/RestfulBookerBookingApi.java
+++ src/test/java/ru/qa/api/restfulbooker/RestfulBookerBookingApi.java
@@ -2,17 +2,18 @@ package ru.qa.api.restfulbooker;
 
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import ru.qa.dto.restfulBooker.request.BookingRequest;
 
 import static io.restassured.RestAssured.given;
 import static ru.qa.specs.ApiSpecs.*;
 
 public class RestfulBookerBookingApi {
-    private RestfulBookerBookingApi() {throw new AssertionError("Utility class");
+    private RestfulBookerBookingApi() {
+        throw new AssertionError("Utility class");
     }
+
     public static Response createBooking(BookingRequest request) {
-
-
 
         return given()
                 .spec(restfulBookRequestSpec())
@@ -23,5 +24,27 @@ public class RestfulBookerBookingApi {
                 .then()
                 .log().all()
                 .extract().response();
+    }
+
+    public static Response updateBooking(BookingRequest request, RequestSpecification authSpec, int bookingId) {
+        return given()
+                .spec(authSpec) // auth with inherited method Auth spec
+                .body(request)
+                .when()
+                .put("/booking/" + bookingId)
+                .then()
+                .extract().response();
+    }
+
+    public static Response getBooking(BookingRequest request, int bookingId) {
+
+        return given()
+                .spec(restfulBookRequestSpec())
+                .when()
+                .get("/booking/" + bookingId)
+                .then()
+                .log().all()
+                .extract().response();
+
     }
 }
